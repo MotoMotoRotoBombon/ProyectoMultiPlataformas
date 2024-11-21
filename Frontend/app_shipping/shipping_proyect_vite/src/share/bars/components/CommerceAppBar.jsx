9c746bf179d3bf56d821fbfbdc8968e4e1e666c9
SrWicho13 <149552236+SrWicho13@users.noreply.github.com>
2024-11-21 14:47:27 -0700

    Goku
    se agrego a Goku

--- Frontend/app_shipping/shipping_proyect_vite/src/share/bars/components/CommerceAppBar.jsx
+++ Frontend/app_shipping/shipping_proyect_vite/src/share/bars/components/CommerceAppBar.jsx
@@ -165,7 +165,7 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="../../../../public/images/icono.png" />
+                <Avatar alt="Remy Sharp" src="../../../../public/images/userDefault.png" />
               </IconButton>
             </Tooltip>
             <Menu
